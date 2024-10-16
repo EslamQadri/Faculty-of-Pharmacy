d6b71e055ebc1f47fe757564a4cf5f572cf5e18b
EslamQadri <eslamkadryuwk@gmail.com>
2024-10-17 00:27:39 +0300

    update super user privilege

--- elearning/middleware.py
+++ elearning/middleware.py
@@ -9,14 +9,15 @@ class SubscriptionCheckMiddleware(MiddlewareMixin):
         if request.path == reverse('subscription_expired'):
             return None
         if request.user.is_authenticated:
+            if not request.user.is_superuser:
            
-            try:
-                user_expiry = UserExpiry.objects.get(user=request.user)
-                
-                if not request.user.is_superuser:
+                try:
+                    user_expiry = UserExpiry.objects.get(user=request.user)
+                    
                     if not user_expiry.is_subscription_active():
 
                         return redirect('subscription_expired')
-            except UserExpiry.DoesNotExist:
-                return redirect('subscription_expired')
+                except UserExpiry.DoesNotExist:
+                    return redirect('subscription_expired')
+            return None 
         return None
