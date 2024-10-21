d04b7cd6075c59772151934efe60c72f5dd776fa
EslamQadri <eslamkadryuwk@gmail.com>
2024-10-21 13:59:22 +0300

    add settings

--- server/settings.py
+++ server/settings.py
@@ -137,3 +137,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "server/static")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
