6a7c8dd2dae5213aadb78bda42fb220f79a57c08
EslamQadri <eslamkadryuwk@gmail.com>
2025-01-04 21:44:05 +0200

    make DEBUG True to get server errors

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "1wh*#8pu7xzcb+%rn-+rak1xgdoa(ac%inl$%aoi2md2gz^y=9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 LOGIN_URL = "login"
